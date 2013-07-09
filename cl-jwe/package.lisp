a70c4f87e985be17aa5d8fad684f8ed13d062f31
Keith Johnson <kjcjohnson@ymail.com>
2013-07-09 12:19:34 -0400

    Added cl-jwe section and package definition

---
+++ cl-jwe/package.lisp
@@ -0,0 +1,10 @@
+;;;CL-JWE, the Common Lisp Java Windowing Environment
+;;;Written by Keith Jens Carl Johnson, 2013
+
+;;;This file contains the package definition for the system:
+
+(in-package "COMMON-LISP-USER")
+
+(defpackage "cl-jwe" 
+  (:nicknames "jwe"))
+  
